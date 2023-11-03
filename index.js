91f4426d5c2de2f9a65142795023ee6bcc8b4e0c
Progalt <henryrgronow@gmail.com>
2023-11-03 17:58:49 +0000

    Redo

--- index.js
+++ index.js
@@ -17,3 +17,57 @@ function scrollToPosts()
         behavior: 'smooth'
     })
 }
+
+function scrollToAbout()
+{
+    const element = document.getElementById("detailed-about")
+
+    element.scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+
+var titleText = "Hi, I'm Henry. "
+var titleTextEmphasis = "Henry"
+
+var typeSpeed = 80
+var typeProgress = 0
+
+var cursorSpan = "<span class=\"cursor\"></span>"
+
+
+function insertIntoString(base, index, str) {
+    newStr = base.slice(0, index) + str + base.slice(index)
+    return newStr
+}
+
+function typeTitle() 
+{
+
+    if (typeProgress < titleText.length) 
+    {
+        var elem = document.getElementById("mainTitle")
+
+        if (typeProgress >= titleText.indexOf(titleTextEmphasis))
+        {
+            var elem = document.getElementById("name")
+        }
+
+        elem.innerHTML = insertIntoString(elem.innerHTML, typeProgress, titleText.charAt(typeProgress))
+
+        typeProgress++
+
+        setTimeout(typeTitle, typeSpeed)
+    }
+    else 
+    {
+        //var elem = document.getElementById("mainTitle")
+        //elem.innerHTML += cursorSpan
+    }
+}
+
+window.onload = function() {
+    var elem = document.getElementById("mainTitle")
+    elem.innerHTML += cursorSpan
+    typeTitle()
+}
